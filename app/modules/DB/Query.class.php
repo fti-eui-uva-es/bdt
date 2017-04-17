a99acc38f178961fe8c4daef5350611ceed75a14
carloscuesta97 <carloscues97@gmail.com>
2017-04-17 17:54:28 +0200

    Mejora del commit #1
    Añadidos métodos y atributos a las clases del kernel del motor

--- app/modules/DB/Query.class.php
+++ app/modules/DB/Query.class.php
@@ -1,5 +1,39 @@
 <?php
 class Query {
-	// TODO: Implement methods
+	
+	private $cache = [];
+	private $result;
+	public $length = 0;
+	public $success = false;
+	
+	
+	/**
+	 * Construct
+	 *
+	 * @param {String} SQL
+	 */
+	public function __construct($sql) {
+		// TODO
+	}
+	
+	
+	/**
+	 * Destruct
+	 */
+	public function __destruct() {
+		// TODO
+	}
+	
+	
+	/**
+	 * Get result
+	 *
+	 * @param {int} Index
+	 * @return {Array} MySQL row
+	 */
+	public function get($index) {
+		// TODO
+	}
+	
 }
 ?>
--- app/modules/DB/class.php
+++ app/modules/DB/class.php
@@ -1,5 +1,34 @@
 <?php
 class DB extends Module {
-	// TODO: Implement methods for database
+	
+	private static $connection = NULL;
+	
+	/**
+	 * Query
+	 *
+	 * @param {String} MySQL query
+	 * @param {Array} [Parameters] 
+	 * @return {Query} Result
+	 */
+	public static function query($query, $params=[]) {
+		// TODO
+	}
+	
+	
+	/**
+	 * Init
+	 */
+	public static function __init() {
+		// TODO: initialize connection
+	}
+	
+	
+	/**
+	 * Shutdown
+	 */
+	public static function __shutdown() {
+		// TODO: close connection
+	}
+	 
 }
 ?>
