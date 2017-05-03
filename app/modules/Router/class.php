9112de119ee290dc9449851539cc39b5bed0210f
gesio1996 <gesio1996@hotmail.com>
2017-05-03 19:59:24 +0200

    Mejora de todo by @carlosbqs
    Se ha finalizado el módulo DB y hemos empezado el módulo Router

--- app/modules/Router/class.php
+++ app/modules/Router/class.php
@@ -1,5 +1,28 @@
 <?php
 class Router extends Module {
-	// TODO: Implement methods
+	
+	/**
+	 * Get parameter
+	 *
+	 * @param {mixed} Index
+	 * @param {String} Type
+	 */
+	public function getParam($index, $type="PATH") {
+		// Get value
+		$res = NULL;
+		if ($type == "GET") {
+			if (isset($_GET[$index])) $res = $_GET[$index];
+		} elseif ($type == "POST") {
+			if (isset($_POST[$index])) $res = $_POST[$index];
+		} else {
+			// TODO que esto es insufrible
+		}
+		
+		// Sanitize value
+		// TODO
+		
+		// Return value
+		return $res;
+	}
 }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 require_once __DIR__ . "/app/init.php";
+
+/*header("Content-Type: text/plain");
+$res = DB::query(sprintf("SELECT * FROM ciclista WHERE nomequipo=%s",
+	DB::sanitize($_GET['equipo'], "text")));
+var_dump($res);*/
 ?>
