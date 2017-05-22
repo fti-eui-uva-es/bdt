d4dee69f26d5f12b93c0af0c728ce0ae473e9ea4
AdrianM97 <adrianmanzanos@gmail.com>
2017-05-22 16:59:29 +0200

    Update INSTALL.php

--- app/INSTALL.php
+++ app/INSTALL.php
@@ -11,60 +11,21 @@ if (count($tables['rows']) > 0) {
 }
 
 // Create tables
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 22-05-2017 a las 16:35:45
--- Versión del servidor: 10.1.19-MariaDB
--- Versión de PHP: 7.0.13
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `banco_del_tiempo`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `acceptservice`
---
-
-CREATE TABLE `acceptservice` (
+DB::query('CREATE TABLE `acceptservice` (
   `acceptService_user_id` int(11) NOT NULL,
   `acceptService_service_id` int(11) NOT NULL,
   `acceptService_done` tinyint(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `commentary`
---
-
-CREATE TABLE `commentary` (
+DB:: query('CREATE TABLE `commentary` (
   `commentary_id` int(11) NOT NULL,
   `commentary_opinion` varchar(5000) NOT NULL,
   `commentary_user_id` int(11) NOT NULL,
   `commentary_service_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8');
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `incidence`
---
-
-CREATE TABLE `incidence` (
+DB:: query('CREATE TABLE `incidence` (
   `incidence_id` int(11) NOT NULL,
   `incidence_subject` char(100) NOT NULL,
   `incidence_commentary` mediumtext NOT NULL,
@@ -72,15 +33,10 @@ CREATE TABLE `incidence` (
   `incidence_resolution` char(50) NOT NULL,
   `incidence_user_id` int(11) NOT NULL,
   `incidence_service_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8');
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `service`
---
-
-CREATE TABLE `service` (
+DB:: query('CREATE TABLE `service` (
   `service_id` int(11) NOT NULL,
   `service_name` char(50) NOT NULL,
   `service_description` varchar(5000) NOT NULL,
@@ -94,15 +50,10 @@ CREATE TABLE `service` (
   `service_time` int(11) NOT NULL,
   `service_endDate` int(11) NOT NULL,
   `service_cancelationDate` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8');
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `user`
---
-
-CREATE TABLE `user` (
+DB:: query('CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `user_nick` char(50) NOT NULL,
   `user_name` char(50) NOT NULL,
@@ -116,51 +67,29 @@ CREATE TABLE `user` (
   `user_expelled` tinyint(1) NOT NULL,
   `user_time` int(11) NOT NULL,
   `user_metadata` varchar(5000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8');
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `acceptservice`
---
-ALTER TABLE `acceptservice`
-  ADD PRIMARY KEY (`acceptService_service_id`);
+DB:: query('ALTER TABLE `acceptservice`
+  ADD PRIMARY KEY (`acceptService_service_id`)');
 
---
--- Indices de la tabla `commentary`
---
-ALTER TABLE `commentary`
-  ADD PRIMARY KEY (`commentary_id`);
 
---
--- Indices de la tabla `incidence`
---
-ALTER TABLE `incidence`
-  ADD PRIMARY KEY (`incidence_id`);
+DB::query('ALTER TABLE `commentary`
+  ADD PRIMARY KEY (`commentary_id`)');
 
---
--- Indices de la tabla `service`
---
-ALTER TABLE `service`
-  ADD PRIMARY KEY (`service_id`);
 
---
--- Indices de la tabla `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
+DB:: query('ALTER TABLE `incidence`
+  ADD PRIMARY KEY (`incidence_id`)');
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
+DB::quety('ALTER TABLE `service`
+  ADD PRIMARY KEY (`service_id`)');
 
---
--- AUTO_INCREMENT de la tabla `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+DB::query('ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`)');
+
+
+DB: query('ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT');
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
